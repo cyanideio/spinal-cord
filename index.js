35f85eae9050d84d44797ecf26ff7d976ae44738
Douglas Eugene Reisinger II <palodequeso@gmail.com>
2016-10-05 15:07:49 -0400

    one minor fix, before I can test it in small work project

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const AjaxMethod = require('src/ajax.js');
-const Collection = require('src/collection.js');
-const Model = require('src/model.js');
-const Router = require('src/router.js');
-const Timer = require('src/timer.js');
-const View = require('src/view.js');
+const AjaxMethod = require('./src/ajax.js');
+const Collection = require('./src/collection.js');
+const Model = require('./src/model.js');
+const Router = require('./src/router.js');
+const Timer = require('./src/timer.js');
+const View = require('./src/view.js');
 
 module.exports = {
     AjaxMethod: AjaxMethod,
