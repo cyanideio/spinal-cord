f81d1d01f9f9de49fccbd21952c35b1f13b8c500
Frank <knarfytrebil@gmail.com>
2016-12-11 21:04:47 +0800

    update tastypie models

--- src/restful/tastypie.js
+++ src/restful/tastypie.js
@@ -4,18 +4,11 @@ const RestfulModel = require('./model.js')
 class TastypieModel extends RestfulModel {
 
     parse(resp, options){
-    	if (resp.hasOwnProperty('objects')){
-    		if (resp.objects.length == 1) {
-    			return resp.objects[0]
-    		}
-			if (resp.objects.length == 0) {
-    			return null
-    		} else {
-    			throw new Error('multiple results returned')
-    		}
-    	} else {
-    		return resp
-    	}
+        if (resp.hasOwnProperty('objects')){
+          return resp.length ? resp[0] : null
+        } else {
+          return resp
+        }
     }
 		
 }
