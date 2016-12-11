bb201120b78428854f63d43eb40c069f40ec0d30
Frank <knarfytrebil@gmail.com>
2016-12-11 21:05:26 +0800

    fix tastypie model

--- src/restful/tastypie.js
+++ src/restful/tastypie.js
@@ -5,7 +5,7 @@ class TastypieModel extends RestfulModel {
 
     parse(resp, options){
         if (resp.hasOwnProperty('objects')){
-          return resp.length ? resp[0] : null
+          return resp.length ? resp.objects[0] : null
         } else {
           return resp
         }
