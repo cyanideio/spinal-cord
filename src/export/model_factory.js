1a8b420ef442da1ad043bc35eaa2d26dbde8f205
YaaMe <zhujie@zhujiedeMacBook-Pro.local>
2017-02-26 21:30:52 +0800

    fix bug

--- src/export/model_factory.js
+++ src/export/model_factory.js
@@ -1,12 +1,12 @@
 'use strict'
 
 module.exports = function(modelConfigs, RestNative) {
-    const ModelBuilder = require('./build_model.js')(RestNative);
+    const ModelBuilder = require('./build_model.js');
     var modelFactory = {}
     var models = Object.keys(modelConfigs)
     models.forEach(function(modelName) {
-        if (/^[A-Z]/.test(modelName)) modelFactory[`${modelName}Model`] = ModelBuilder(modelConfigs[modelName]);
-        else modelFactory[`${modelName}Model`] = new(ModelBuilder(modelConfigs[modelName]));
+        if (/^[A-Z]/.test(modelName)) modelFactory[`${modelName}Model`] = ModelBuilder(modelConfigs[modelName], RestNative);
+        else modelFactory[`${modelName}Model`] = new (ModelBuilder(modelConfigs[modelName], RestNative));
     })
     return modelFactory;
 }
