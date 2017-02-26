06565e7076f25051c03f9ab03349831e0b522418
YaaMe <zhujie@zhujiedeMacBook-Pro.local>
2017-02-26 18:36:42 +0800

    try push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const AjaxMethod = require('./src/backends/ajax')
-const RestNative = require('./src/backends/restful-native')
+// const AjaxMethod = require('./src/backends/ajax')
+// const RestNative = require('./src/backends/restful-native')
 const Collection = require('./src/collection')
 const Model = require('./src/model')
 const RestfulModel = require('./src/restful/model')
@@ -13,10 +13,10 @@ const ModelBuilder = require('./src/export/build_model')
 const ModelFactory = require('./src/export/model_factory')
 
 module.exports = {
-    Backends: {
-		RestNative: RestNative,
-		AjaxMethod: AjaxMethod
-    },
+  //   Backends: {
+		// RestNative: RestNative,
+		// AjaxMethod: AjaxMethod
+  //   },
     Collection: Collection,
     Model: Model,
     Restful: {
