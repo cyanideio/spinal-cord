bb5425c5dcf73d16122cb206cd8b5be91258ac1e
YaaMe <zhujie@zhujiedeMacBook-Pro.local>
2017-03-04 20:15:12 +0800

    fix prop

--- src/export/build_model.js
+++ src/export/build_model.js
@@ -2,14 +2,6 @@
 module.exports = function(config, RestNative, Model) {
     const RestfulModel = Model || require('../restful/model.js')(RestNative);
     class ConfigModel extends RestfulModel {
-        constructor(data) {
-            super(data);
-            if (config.props) {
-                Object.keys(config.props).forEach(function(propName){
-                    Object.defineProperty(this,propName, config.props[propName])
-                })
-            }
-        }
         get resource_name() {
             return config.resource_name;
         }
@@ -18,5 +10,12 @@ module.exports = function(config, RestNative, Model) {
             return config.host;
         }
     }
+
+    if (config && config.props) {
+        Object.keys(config.props).forEach(function(propName) {
+            Object.defineProperty(ConfigModel.prototype, propName, config.props[propName])
+        })
+    }
+
     return ConfigModel;
 }
