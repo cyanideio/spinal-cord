5f43b7eac0d704c1556eed62eb815a8237c7c69d
YaaMe <zhujie@zhujiedeMacBook-Pro.local>
2017-03-04 19:48:46 +0800

    fix some bug

--- src/export/build_model.js
+++ src/export/build_model.js
@@ -1,8 +1,15 @@
 'use strict'
-
-module.exports = function(config, RestNative) {
-    const RestfulModel = require('../restful/model.js')(RestNative);
+module.exports = function(config, RestNative, Model) {
+    const RestfulModel = Model || require('../restful/model.js')(RestNative);
     class ConfigModel extends RestfulModel {
+        constructor(data) {
+            super(data);
+            if (config.props) {
+                Object.keys(config.props).forEach(function(propName){
+                    Object.defineProperty(this,propName, config.props[propName])
+                })
+            }
+        }
         get resource_name() {
             return config.resource_name;
         }
--- src/export/model_factory.js
+++ src/export/model_factory.js
@@ -1,12 +1,12 @@
 'use strict'
 
-module.exports = function(modelConfigs, RestNative) {
+module.exports = function(modelConfigs, RestNative, Model) {
     const ModelBuilder = require('./build_model.js');
     var modelFactory = {}
     var models = Object.keys(modelConfigs)
     models.forEach(function(modelName) {
-        if (/^[A-Z]/.test(modelName)) modelFactory[`${modelName}Model`] = ModelBuilder(modelConfigs[modelName], RestNative);
-        else modelFactory[`${modelName}Model`] = new (ModelBuilder(modelConfigs[modelName], RestNative));
+        if (/^[A-Z]/.test(modelName)) modelFactory[`${modelName}Model`] = ModelBuilder(modelConfigs[modelName], RestNative, Model);
+        else modelFactory[`${modelName}Model`] = new (ModelBuilder(modelConfigs[modelName], RestNative, Model));
     })
     return modelFactory;
 }
